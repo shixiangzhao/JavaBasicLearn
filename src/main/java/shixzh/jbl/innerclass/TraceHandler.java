7eae342d341dd890f847785dc714ec3c3cd28513
shixiangzhao <zsxfordream@163.com>
2017-05-15 21:07:43 +0800

    proxy test

---
+++ src/main/java/shixzh/jbl/innerclass/TraceHandler.java
@@ -0,0 +1,34 @@
+package shixzh.jbl.innerclass;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class TraceHandler implements InvocationHandler {
+
+    private Object target;
+
+    public TraceHandler(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        // print implicit argument
+        System.out.print(target);
+        // print method name
+        System.out.print("." + method.getName() + "(");
+        // print explicit arguments
+        if (args != null) {
+            for (int i = 0; i < args.length; i++) {
+                System.out.print(args[i]);
+                if (i < args.length - 1) {
+                    System.out.print(", ");
+                }
+            }
+        }
+        System.out.println(")");
+        // invoke actual method
+        return method.invoke(target, args);
+    }
+
+}
